0e9f40e32cd51283b3cc9fc43e20703a44c9720d
Naoki Kinoshita <uttnaoki@gmail.com>
2017-12-20 13:38:32 +0900

    InfoBox のタグのデザインを修正

--- public/javascripts/infoBox.js
+++ public/javascripts/infoBox.js
@@ -2,10 +2,10 @@ function appendPlacesTag(places, boxname) {
   for (const p of places) {
     const this_id = boxname + '_' + p.name;
 
-    $('#' + boxname).append('<div id="' + this_id + '"'
+    $('#' + boxname).append('<p id="' + this_id + '"'
       + ' class="' + boxname + '_tag">'
-      + p.name + '</div>')
-      
+      + p.name + '</p>')
+
     $('#' + this_id).on('click', function() {
       // wikipedia のページへ移動
       window.location.href = 'https://ja.wikipedia.org/wiki/' + p.name;
