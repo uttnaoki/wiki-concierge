ab819b9a41d04e5f61bf9d1e2a94932fc1d3d8f1
Naoki Kinoshita <uttnaoki@gmail.com>
2018-01-18 22:05:37 +0900

    画像取得処理のバグを修正

--- util/getWikiData.py
+++ util/getWikiData.py
@@ -98,11 +98,13 @@ def saveImg(place, soup, img):
         img.save(img_path)
         return;
     if("commons/thumb/1/1b/Gthumb.svg" in imgurl):
+        img.save(img_path)
         return;
     imghtml = urllib.request.urlopen(imglink).read()
     imgsoup = BeautifulSoup(imghtml, 'lxml')
     imgtable = imgsoup.find("table", class_="layouttemplate licensetpl mw-content-ltr")
     if(imgtable == None):
+        img.save(img_path)
         return;
     text = imgtable.find("a").text
     if(text == "クリエイティブ・コモンズ"):
@@ -114,6 +116,7 @@ def saveImg(place, soup, img):
     urllib.request.urlretrieve(imgurl,img_path)
 
 def getPlaceData(place, img):
+    print(place)
     url = makeWikiURL(place)
     html = urllib.request.urlopen(url).read()
     html = re.sub(r'<sup.*?</sup>', '',html.decode("utf-8"))
@@ -129,5 +132,6 @@ def getPlacesData(places):
 
 if __name__ == '__main__':
     # title = ["後楽園","倉敷美観地区"]
-    title = ["後楽園","倉敷美観地区","岡山城","吉備津神社","最上稲荷","鬼ノ城","鷲羽山ハイランド","井倉洞","満奇洞","湯原温泉","湯郷温泉","津山城","ドイツの森","吹屋ふるさと村郷土館","旧矢掛本陣石井家","奥津渓","美星町"]
+    # title = ["後楽園","倉敷美観地区","岡山城","吉備津神社","最上稲荷","鬼ノ城","鷲羽山ハイランド","井倉洞","満奇洞","湯原温泉","湯郷温泉","津山城","ドイツの森","吹屋ふるさと村郷土館","旧矢掛本陣石井家","奥津渓","美星町"]
+    title = ["満奇洞","湯原温泉","湯郷温泉","津山城"]
     wiki_dataset = getPlacesData(title)
