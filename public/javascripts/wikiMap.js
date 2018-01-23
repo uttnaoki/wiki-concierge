37a58e76f3bffdf71b5c3b5e70a95e5812165c51
Naoki Kinoshita <uttnaoki@gmail.com>
2018-01-23 23:56:09 +0900

    記事評価の指標を増やし，wikiMapページ に反映

--- public/javascripts/wikiMap.js
+++ public/javascripts/wikiMap.js
@@ -4,39 +4,26 @@ const icon_filepath = '/images/icon1.png';
 const icon_filepath_phone = '';
 let status_select_marker = { flag: 0, name: '' };
 
-function calScore_subfunc(value, base_value, result_set) {
-  let i = 0;
-  while (i < base_value.length) {
-    if (value < base_value[i]) {
-      return result_set[i];
-    }
-    i++;
-  }
-  return result_set[i];
-}
-
-function calScore(value, type) {
+function calScore(Scores, type) {
   let result = 0;
-  const base_value = [1000, 2000, 3000, 5000, 10000];
   const result_set = {
     size: [30, 45, 60, 90, 120, 150],
     opacity: [0.5, 0.5, 0.75, 0.75, 1, 1.5]
   }
-  switch (type) {
-    case 'size':
-      result = calScore_subfunc(value, base_value, result_set.size);
-      break;
-    case 'opacity':
-      result = calScore_subfunc(value, base_value, result_set.opacity);
-      break;
-    default:
-      console.log('"' + type + '" に対する calScore() 処理は定義されていません。');
+
+  let scores_sum = 0;
+  let scores_len = 0;
+  for (var key in Scores) {
+    scores_sum += Scores[key];
+    scores_len += 1;
   }
-  return result;
+  const scores_mean = scores_sum / scores_len;
+
+  return result_set[type][Math.floor(scores_mean)];
 }
 
-function setIconOption(value) {
-  const size = calScore(value, 'size');
+function setIconOption(Scores) {
+  const size = calScore(Scores, 'size');
   const icon_pc = icon_filepath;
   const icon_phone = icon_filepath_phone ? icon_filepath_phone : icon_pc;
   let Icon = L.icon({
@@ -49,7 +36,7 @@ function setIconOption(value) {
   return Icon;
 }
 
-function makePopupMessage(name, value, overview) {
+function makePopupMessage(name, overview) {
   const message =
     '<h2>' +
     '<a href="https://ja.wikipedia.org/wiki/' + name + '" target="_blank">' + name + '</a>' +
@@ -72,15 +59,22 @@ function highlightMarker(name, click_flag) {
 }
 
 function putMarker(map, data) {
-  const Icon = setIconOption(data.value);
+  const Scores = {
+    Score_countFulltext: data.Score_countFulltext,
+    Score_countLang: data.Score_countLang,
+    Score_itemCount: data.Score_itemCount,
+    Score_editfrequency: data.Score_editfrequency,
+    Score_countperson: data.Score_countperson
+  }
+  const Icon = setIconOption(Scores);
   var marker = L.marker(
       [data.lat, data.lng], {
         icon: Icon,
-        opacity: calScore(data.value, 'opacity'),
+        opacity: calScore(Scores, 'opacity'),
       }
     )
     .addTo(map)
-    .bindPopup(makePopupMessage(data.name, data.value, data.overview))
+    .bindPopup(makePopupMessage(data.name, data.overview))
 
   marker.on("mouseover", function(e) {
     highlightMarker(data.name)
@@ -150,7 +144,6 @@ function drawMap(dataset) {
   ).addTo(map);
   var info_class_type = 0;
   for (var d of dataset) {
-    // console.log(Math.log(d.value));
     marker_set[d.name] = putMarker(map, d);
     appendInfoTag(d, info_class_type);
     info_class_type = Number(!info_class_type);
